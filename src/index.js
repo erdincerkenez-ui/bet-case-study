23d0bd122a647bbd0edb121aa6aa8d0806022791
Erdinç Erkenez <erdincerkenez@192.168.1.108>
2024-11-18 18:51:17 +0300

    compile react for the browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('test');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <h1>bet case study2</h1>
+    )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    module: {
-        rules: [
-            {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    open: true
+  },
 };
